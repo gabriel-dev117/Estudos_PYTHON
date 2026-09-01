f59e1f70d5dcc1f7e96c5e307760527cca93da97
Gabriel Lima <dev.gabriel.lima.117@gmail.com>
2026-09-01 20:35:08 -0300

    Recursividade

---
+++ 4-5_(funcoes_recursivas).py
@@ -0,0 +1,19 @@
+# Funções recursivas e recursividade
+# - funções que podem se chamar de volta
+# - úteis p/ dividir problemas grandes em partes menores
+# Toda função rescursiva deve ter:
+# - Um problema que possa ser dividido em partes menores
+# - Um caso recursivo que resolve o pequeno problema
+# - Um caso base que para a recursão
+# - fatorial - n! = 5 * 4 * 3 * 2 * 1 = 120
+
+def recursiva(inicio = 0, fim = 5):
+    if inicio >= fim:
+        return fim
+
+    print(inicio, fim)
+
+    inicio += 1
+    return recursiva(inicio, fim)
+
+print(recursiva())
